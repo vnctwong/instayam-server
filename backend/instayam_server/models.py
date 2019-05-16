85a1450517cea0a44e14725f9316afee1e9eb89d
VWMAC <vincentkywong@gmail.com>
2019-05-16 16:01:41 -0700

    user model setup admin set up can create new users

--- backend/instayam_server/models.py
+++ backend/instayam_server/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=120)
+    email = models.CharField(max_length=120)
+    password = models.CharField(max_length=120)
+    is_private = models.BooleanField(default=False)
+
+    def _str_(self):
+        return self.name
