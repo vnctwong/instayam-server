b5c9469a74436f1e0c414f4586c2c8b619aba025
VWMAC <vincentkywong@gmail.com>
2019-05-17 13:17:51 -0700

    admin and api endpoints functional

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -121,7 +121,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# we whitelist localhost:3000 because that's where frontend will be served
-    CORS_ORIGIN_WHITELIST = (
-         'localhost:3000/'
-     )
+# whitelist localhost:3000 for frontend
+# CORS_ORIGIN_WHITELIST = ('localhost:3000/')
